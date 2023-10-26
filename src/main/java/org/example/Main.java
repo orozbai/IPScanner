505f3638e8f1b9fc95fa257dd358b50b2204034c
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-10-26 15:22:45 +0600

    git init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import io.javalin.Javalin;
+
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
