31d627cbf8bf8c4204197583052a81311394650b
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-10-26 15:28:40 +0600

    added code to Main to get the path and process it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,18 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", context -> {
+            String htmlContent = new String(Files.readAllBytes(Paths.get("src/web/index.html")));
+            context.contentType("text/html").result(htmlContent);
+        });
 
+        app.post("/scan", context -> {
+            String ipAddressStr = context.formParam("ipAddress");
+            InetAddress ipAddress = InetAddress.getByName(ipAddressStr);
+            int num = Integer.parseInt(context.formParam("numThreads"));
+
+
+        });
     }
 }
